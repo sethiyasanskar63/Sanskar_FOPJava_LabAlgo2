99a1bf952af87bf0d84e8cc977af85e5d5495112
sethiyasanskar63 <101315790+sethiyasanskar63@users.noreply.github.com>
2022-10-09 14:50:29 +0530

    Add files via upload

---
+++ Sanskar_FOPJava_LabAlgo2/src/com/lab2/q1/paymoney/PayMoney.java
@@ -0,0 +1,54 @@
+package com.lab2.q1.paymoney;
+
+import java.util.Scanner;
+
+public class PayMoney {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the size of transaction array:");
+		int size = sc.nextInt();
+
+		System.out.println("Enter the values of array:");
+		int[] transactionArr = new int[size];
+		for (int i = 0; i < size; i++) {
+			transactionArr[i] = sc.nextInt();
+		}
+
+		System.out.println("Enter the number of targets to be achieved:");
+		int target = sc.nextInt();
+
+		while (target > 0) {
+
+			System.out.println("Enter the target value:");
+			int targetValue = sc.nextInt();
+
+			int sum = 0;
+			boolean isAchieved = false;
+
+			for (int i = 0; i < size; i++) {
+
+				sum = sum + transactionArr[i];
+
+				if (targetValue <= sum) {
+
+					System.out.println("Target " + targetValue + " achieved after " + (i + 1) + " transactions");
+					isAchieved = true;
+					break;
+				}
+			}
+
+			if (!isAchieved) {
+				System.out.println("Given target not achieved.");
+			}
+
+			target--;
+		}
+
+		sc.close();
+
+	}
+
+}
